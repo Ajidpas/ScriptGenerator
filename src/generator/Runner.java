7552b199dcd513fe58c051899ea74accad1d77b9
ToSan <ToSan@192.168.0.21>
2016-03-24 01:10:28 +0200

    Write into script file

--- src/generator/Runner.java
+++ src/generator/Runner.java
@@ -1,10 +1,20 @@
 package generator;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Runner {
 	
 	public static void main(String[] args) {
 		NewsTableGen gen = new NewsTableGen();
-		System.out.println(gen.generat(10));
+		String script = gen.generat(10);
+//		System.out.println(gen.generat(10));
+		try (FileWriter fw = new FileWriter("ScriptFile.txt")) {
+			fw.write(script);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("done");
 	}
 
 }
